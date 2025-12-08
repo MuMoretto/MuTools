febe70a42e6d416b52755ee1d2f421ec332c87b6
MuMoretto <murilomoretto21@gmail.com>
2025-12-08 01:13:01 -0300

    Version 0.1.1.1

--- main.py
+++ main.py
@@ -5,28 +5,28 @@ janelaPrincipal.title("MuTools - Versão Python")
 janelaPrincipal.geometry("1400x700")
 
 textoBemVindo = tk.Label(janelaPrincipal, text="Bem-Vindo ao MuTools!")
-textoBemVindo.pack()
+textoBemVindo.grid()
 textoUsuario = tk.Label(janelaPrincipal, text="Digite seu Usuário:")
-textoUsuario.pack()
+textoUsuario.grid()
 entradaUsuario = tk.Entry(janelaPrincipal)
-entradaUsuario.pack()
+entradaUsuario.grid()
 textoSenha = tk.Label(janelaPrincipal, text="Digite sua Senha:")
-textoSenha.pack()
+textoSenha.grid()
 entradaSenha = tk.Entry(janelaPrincipal)
-entradaSenha.pack()
+entradaSenha.grid()
 
 textoLogin = tk.Label(janelaPrincipal, text="")
-textoLogin.pack()
+textoLogin.grid()
 
 def mostrarLogin():
     avisoUsuario = entradaUsuario.get()
     avisoSenha = entradaSenha.get()
     texto = tk.Label(janelaPrincipal, text=f"Você logou como '{avisoUsuario}', com a senha: '{avisoSenha}'...")
-    texto.pack()
-    botaoContinuar.pack()
+    texto.grid()
+    botaoContinuar.grid()
 
 botaoLogar = tk.Button(janelaPrincipal, text="Logar", command=mostrarLogin)
-botaoLogar.pack()
+botaoLogar.grid()
 
 botaoContinuar = tk.Button(janelaPrincipal, text="Continuar")               # Criei o botão aqui e ele só aparece depois da exibição da mensagem de login... O mesmo foi chamado na função "mostrarLogin".
 
