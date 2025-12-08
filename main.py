624042659bbb623da1bfaf9279dc5f520298b526
MuMoretto <murilomoretto21@gmail.com>
2025-12-08 16:51:46 -0300

    Version 0.1.2

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
 import tkinter as tk
+from program_functions.menu_functions import abrirMenu
+
+usuarios = [
+    ("test", "123"),
+    ("Murilo", "murilo2005")
+]
 
 janelaPrincipal = tk.Tk()
 janelaPrincipal.title("MuTools - Versão Python")
-janelaPrincipal.geometry("1400x700")
+janelaPrincipal.geometry("900x450")
 
 textoBemVindo = tk.Label(janelaPrincipal, text="Bem-Vindo ao MuTools!")
-textoBemVindo.grid()
+textoBemVindo.grid(row=0, column=0, padx=10, pady=10)
 textoUsuario = tk.Label(janelaPrincipal, text="Digite seu Usuário:")
-textoUsuario.grid()
+textoUsuario.grid(row=1, column=0, padx=10, pady=10)
 entradaUsuario = tk.Entry(janelaPrincipal)
-entradaUsuario.grid()
+entradaUsuario.grid(row=2, column=0, padx=10, pady=0)
 textoSenha = tk.Label(janelaPrincipal, text="Digite sua Senha:")
-textoSenha.grid()
+textoSenha.grid(row=3, column=0, padx=10, pady=10)
 entradaSenha = tk.Entry(janelaPrincipal)
-entradaSenha.grid()
+entradaSenha.grid(row=4, column=0, padx=10, pady=0)
 
 textoLogin = tk.Label(janelaPrincipal, text="")
-textoLogin.grid()
+textoLogin.grid(row=6, column=0, padx=10, pady=10)
 
 def mostrarLogin():
-    avisoUsuario = entradaUsuario.get()
-    avisoSenha = entradaSenha.get()
-    texto = tk.Label(janelaPrincipal, text=f"Você logou como '{avisoUsuario}', com a senha: '{avisoSenha}'...")
-    texto.grid()
-    botaoContinuar.grid()
+    usuario = entradaUsuario.get()
+    senha = entradaSenha.get()
+
+    if (usuario, senha) in usuarios:
+        textoLogin.config(text=f"Você logou como '{usuario}', com a senha '{senha}'")
+        botaoContinuar.grid(row=7, column=0, padx=10, pady=10)
+    else:
+        textoLogin.config(text="Usuário ou senha incorretos! Tente novamente...")
 
 botaoLogar = tk.Button(janelaPrincipal, text="Logar", command=mostrarLogin)
-botaoLogar.grid()
+botaoLogar.grid(row=5, column=0, padx=10, pady=20)
 
-botaoContinuar = tk.Button(janelaPrincipal, text="Continuar")               # Criei o botão aqui e ele só aparece depois da exibição da mensagem de login... O mesmo foi chamado na função "mostrarLogin".
+botaoContinuar = tk.Button(janelaPrincipal, text="Continuar", command=lambda: abrirMenu(janelaPrincipal))               # Criei o botão aqui e ele só aparece depois da exibição da mensagem de login... O mesmo foi chamado na função "mostrarLogin".
 
 janelaPrincipal.mainloop()
---
+++ program_functions/menu_functions.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+def abrirMenu(janelaPrincipal):
+    janelaPrincipal.withdraw()
+
+    janelaMenu = tk.Toplevel()
+    janelaMenu.title("MuTools - Menu Inicial")
+    janelaMenu.geometry("900x450")
+
+    textoEscolhaOpcao = tk.Label(janelaMenu, text="Escolha uma opção abaixo:")
+    textoEscolhaOpcao.grid(row=0, column=0, padx=10, pady=10)
+
+    botaoCalculadora = tk.Button(janelaMenu, text="Calculadora")
+    botaoCalculadora.grid(row=1, column=0, padx=10, pady=10)
+
+    botaoSobre = tk.Button(janelaMenu, text="Sobre")
+    botaoSobre.grid(row=2, column=0, padx=10, pady=10)
